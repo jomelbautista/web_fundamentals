271eb017e9cf6a6d0ce691e405986bdb5afe7c84
Jomel Bautista <jomelbautista@Jomels-MBP.attlocal.net>
2022-05-10 21:33:32 -0700

    added past projects

---
+++ succulents/script.js
@@ -0,0 +1,6 @@
+const targetDiv = document.getElementById('cookies-row');
+const btn = document.getElementById('btn-accept');
+
+btn.onclick = function () {
+    targetDiv.style.display = "none";
+};
